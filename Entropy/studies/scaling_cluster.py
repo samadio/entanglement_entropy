d8137645a4c58f558050ada4ca50084f71391134
Simone Amadio <amadiosimone1996@gmail.com>
2020-05-13 18:57:25 +0200

    scaling_cluster.py working

--- Entropy/studies/scaling_cluster.py
+++ Entropy/studies/scaling_cluster.py
@@ -1,6 +1,6 @@
-
 from time import time
 from states import *
+
 total_start = time()
 
 numbers = [21, 33]#, 66, 131]
@@ -8,8 +8,8 @@ L_list = [aux.lfy(N) for N in numbers]
 Y = 13
 current_state = 0
 number_of_bip = 10
-means = [np.ones((len(numbers), 2 * L), dtype=float) * 512.32 for L in L_list]
-stds = [np.ones((len(numbers), 2 * L), dtype=float) * 512.32 for L in L_list]
+means = [np.ones(2 * L, dtype=float) * 512.32 for L in L_list]
+stds = [np.ones(2 * L, dtype=float) * 512.32 for L in L_list]
 
 [print(mean.shape) for mean in means]
 
@@ -27,14 +27,13 @@ for i, N in enumerate(numbers):
             entropy = entanglement_entropy_from_state(current_state, chosen, sparse=False)
             times.append(time() - local_start)
 
-        print(i,k)
-        means[i][k - 1, :] = np.mean(times)
-        stds[i][k - 1, :] = np.sqrt(np.var(times))
+        means[i][k - 1] = np.mean(times)
+        stds[i][k - 1] = np.sqrt(np.var(times))
 
-total_time = start - time()
+total_time = time() - start
 
 files = [open("times_L=" + str(L) + ".txt", 'a+') for L in L_list]
-[print(str(number_of_bip) + "bipartitions\nMeans: \n" + str(means[i]), file=files[i]) for i in range(len(numbers))]
+[print(str(number_of_bip) + " bipartitions\nMeans: \n" + str(means[i]), file=files[i]) for i in range(len(numbers))]
 [print("stds: \n" + str(stds[i]), file=files[i]) for i in range(len(numbers))]
-print("total time" + str(total_time), file=files[0])
+print("total time " + str(total_time), file=files[0])
 [file.close() for file in files]
