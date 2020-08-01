f5e7687406908f28db443a5c5884d22d5b503112
Simone Amadio <amadiosimone1996@gmail.com>
2020-08-01 08:20:29 +0200

    L=5 augmented

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=8entropy       # The name of your job, you'll se it in squeue.
+#SBATCH --job-name=4entropy       # The name of your job, you'll se it in squeue.
 #SBATCH --mail-type=ALL              # Mail events (NONE, BEGIN, END, FAIL, ALL). Sends you an email when the job begins, ends, or fails; you can combine options.
 #SBATCH --mail-user=samadio@sissa.it    # Where to send the mail
 #
 # ---- CPU resources configuration  ----  |  Clarifications at https://slurm.schedmd.com/mc_support.html
 #
 #SBATCH --ntasks=1                   # Number of MPI ranks (1 for MPI serial job)
-#SBATCH --cpus-per-task=10           # Number of threads per MPI rank (MAX: 2x32 cores on _partition_2, 2x20 cores on _partition_1)
+#SBATCH --cpus-per-task=4           # Number of threads per MPI rank (MAX: 2x32 cores on _partition_2, 2x20 cores on _partition_1)
 #SBATCH --nodes=1                    # Number of nodes
 #SBATCH --ntasks-per-node=1          # How many tasks on each node
 #SBATCH --ntasks-per-socket=1        # How many tasks on each socket
