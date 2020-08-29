7f34f86870b89cce8340108c994caf91c27bc145
Simone Amadio <amadiosimone1996@gmail.com>
2020-08-29 16:24:53 +0200

    construction vs eigval time with numba

--- Entropy/src/scaling_cluster.py
+++ Entropy/src/scaling_cluster.py
@@ -2,6 +2,10 @@ from time import time
 from states import *
 from entropies import *
 
+import cupy as cp
+from cupy.linalg import eigvalsh as gpu_eigh
+
+
 total_start = time()
 
 numbers = [23, 35, 68,132]#[23, 35, 68, 132]
@@ -9,45 +13,74 @@ L_list = [aux.lfy(N) for N in numbers]
 Y = 13
 current_state = 0
 number_of_bip = 1_000
-means = [np.ones(2 * L + 1, dtype=float) * 512.32 for L in L_list]
-stds = [np.ones(2 * L + 1, dtype=float) * 512.32 for L in L_list]
+constr_means = [np.ones(2 * L + 1, dtype=float) * 512.32 for L in L_list]
+constr_stds = [np.ones(2 * L + 1, dtype=float) * 512.32 for L in L_list]
+eigh_means = [np.ones(2 * L + 1, dtype=float) * 512.32 for L in L_list]
+eigh_stds = [np.ones(2 * L + 1, dtype=float) * 512.32 for L in L_list]
 
 start = time()
 for i, N in enumerate(numbers):
     flag = False
     L = L_list[i]
+    nonzeros = aux.nonzeros_decimal(2 * L, Y, N)
     for k in range(1, 2 * L + 1):
-        current_state = construct_modular_state(k, L, aux.nonzeros_decimal(k, Y, N)).toarray().flatten()
+        current_state = construct_modular_state(k, L, nonzeros[:2**k]).toarray().flatten()
         number_qubits = k + L
         bipartitions = [bip.random_bipartition(range(number_qubits), number_qubits // 2) for i in range(number_of_bip)]
-        times = []
+        constr_times = []
+        eigh_times = []
         for chosen in bipartitions:
             local_start = time()
-            entropy = entanglement_entropy_from_state(current_state, chosen, sparse=False, gpu=True)
-            if flag: times.append(time() - local_start)
-            else: flag = True
+            notchosen = bip.notchosen(chosen, number_qubits)
+            W = W_from_state_numba(current_state, chosen, notchosen)
+            if flag: constr_times.append(time() - local_start)
+            else: pass
+            local_start = time()
+            W = cp.array(W)
+            rho = W.dot(W.conj().T)
+            eig = gpu_eigh(rho)
+            eig = eig[eig > 1e-5]
+            a = cp.log2(eig)
+            entr = cp.asnumpy(- cp.sum(eig * a))
+            if flag: eigh_times.append(time() - local_start)
+            else: flag = True       
 
-        means[i][k - 1] = np.mean(times)
-        stds[i][k - 1] = np.sqrt(np.var(times))
+        constr_means[i][k - 1] = np.mean(constr_times)
+        constr_stds[i][k - 1] = np.sqrt(np.var(constr_times))
+        eigh_means[i][k - 1] = np.mean(eigh_times)
+        eigh_stds[i][k - 1] = np.sqrt(np.var(eigh_times))
 
     current_state = apply_IQFT(L, current_state)
     bipartitions = [bip.random_bipartition(range(number_qubits), number_qubits // 2) for i in range(number_of_bip)]
-    times = []
+    constr_times = []
+    eigh_times = []
     for chosen in bipartitions:
         local_start = time()
-        entropy = entanglement_entropy_from_state(current_state, chosen, sparse=False, gpu=True)
-        if flag: times.append(time() - local_start)
-        else: flag = True
+       	notchosen = bip.notchosen(chosen, number_qubits)                    
+        W = W_from_state_numba(current_state, chosen, notchosen)
+        if flag: constr_times.append(time() - local_start)
+        else: pass       
+       	local_start = time()
+       	W = cp.array(W)
+        rho = W.dot(W.conj().T)
+        eig = gpu_eigh(rho)
+        eig = eig[eig > 1e-5]
+        a = cp.log2(eig)
+        entr = cp.asnumpy(- cp.sum(eig * a))
+        if flag: eigh_times.append(time() - local_start)
+        else: flag = True 
 
-    means[i][-1] = np.mean(times)
-    stds[i][ 1] = np.sqrt(np.var(times))
+    constr_means[i][- 1] = np.mean(constr_times)
+    constr_stds[i][- 1] = np.sqrt(np.var(constr_times))
+    eigh_means[i][- 1] = np.mean(eigh_times)
+    eigh_stds[i][- 1] = np.sqrt(np.var(eigh_times))
 
 
 total_time = time() - start
 
-files = [open("/home/samadio/entropy/Entropy/test_times_L_" + str(L) + ".py", 'a+') for L in L_list]
+files = [open("/home/samadio/entropy/Entropy/constr_vs_eig_times_L_" + str(L) + ".py", 'a+') for L in L_list]
 
-[print("#"+str(number_of_bip) + " bipartitions, PARALLEL COMPILING \new_means = " + str(means[i].tolist()), file=files[i]) for i in range(len(numbers))]
-[print("new_stds = " + str(stds[i].tolist()), file=files[i]) for i in range(len(numbers))]
+[print("#"+str(number_of_bip) + " bipartitions, PARALLEL COMPILING \nconstr_means = " + str(constr_means[i].tolist())+ "\neigh_means = " + str(eigh_means[i].tolist()), file=files[i]) for i in range(len(numbers))]
+[print("constr_stds = " + str(constr_stds[i].tolist())+"\neigh_stds = " + str(eigh_stds[i].tolist()), file=files[i]) for i in range(len(numbers))]
 print("#total time " + str(total_time/60) + " mins", file=files[0])
 [file.close() for file in files]
--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=test_times_10      # The name of your job, you'll se it in squeue.
+#SBATCH --job-name=ConstrEighTimes1000Bip40Cores      # The name of your job, you'll se it in squeue.
 #SBATCH --mail-type=END              # Mail events (NONE, BEGIN, END, FAIL, ALL). Sends you an email when the job begins, ends, or fails; you can combine options.
 #SBATCH --mail-user=samadio@sissa.it    # Where to send the mail
 #
