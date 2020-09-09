788460fc4af10a024708ae728d83ed783f01a7c6
Simone Amadio <amadiosimone1996@gmail.com>
2020-09-09 21:05:08 +0200

    Figures reloaded

--- Entropy/1_percent/figure.py
+++ Entropy/1_percent/figure.py
@@ -7,10 +7,11 @@ ax1.hist(results_2[8][1], bins=20, density=True,label="prec2")
 ax2.hist(results_1[8][1],bins=20, density=True,label="prec1")
 plt.legend()
 plt.savefig("test_L8_k9", dpi=300)
+plt.show()
 
 fig, (ax1,ax2) = plt.subplots(1,2)
 ax1.hist(results_2[7][1], bins=20, density=True,label="prec2")
 ax2.hist(results_1[7][1],bins=20, density=True,label="prec1")
 plt.legend()
 plt.savefig("test_L8_k8", dpi=300)
-
+plt.show()
