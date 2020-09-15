c315759878b50699b341c12b1c90cff7996ed524
Simone Amadio <amadiosimone1996@gmail.com>
2020-09-15 18:27:25 +0200

    N24 done

--- Entropy/src/main_cuda_dense_montecarlo.py
+++ Entropy/src/main_cuda_dense_montecarlo.py
@@ -1,18 +1,18 @@
 from entropies import entanglement_entropy_montecarlo
 from states import *
 
-numbers = [257] #, [15, 22,33,66/71,132/131/129]
-maxiter = [1_000] #[1M, 1M, 500k, 50k, 12k]
+numbers = [24] #, [15, 22,33,66/71,132/131/129]
+maxiter = [100_000] #[1M, 1M, 500k, 50k, 12k]
 L_list = [aux.lfy(N) for N in numbers]
 Y = 13
 
-files = [open("/home/samadio/entanglement_entropy/Entropy/1_percent/results_L_" + str(L) + "_" +str(maxiter[0]) +"_N_"+str(numbers[0])+"_new.py", 'a+') for L in L_list]
+files = [open("/home/samadio/entanglement_entropy/Entropy/1_percent/results_L_" + str(L) + "_" +str(maxiter[0]) +"_N_"+str(numbers[0])+".py", 'a+') for L in L_list]
 
 for i, N in enumerate(numbers):
     step = max(maxiter[i]//100, 50)
     print("from numpy import array", file=files[i])
     print("#step: " +str(step), file=files[i])
-    res = entanglement_entropy_montecarlo(Y, N, maxiter=maxiter[i], step=step, tol=1e-2, gpu=False, sparse=False)
+    res = entanglement_entropy_montecarlo(Y, N, maxiter=maxiter[i], step=step, tol=1e-2, gpu=True, sparse=False)
     print("results = " + str(res), file= files[i])
     files[i].close()
     print("lens: ", [len(r[1]) for r in res])
