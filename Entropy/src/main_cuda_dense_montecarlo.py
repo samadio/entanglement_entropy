c0d66de21c595658d68f49ea20bfa5ca10d34c77
Simone Amadio <amadiosimone1996@gmail.com>
2020-09-16 18:52:13 +0200

    N256

--- Entropy/src/main_cuda_dense_montecarlo.py
+++ Entropy/src/main_cuda_dense_montecarlo.py
@@ -1,8 +1,8 @@
 from entropies import entanglement_entropy_montecarlo
 from states import *
 
-numbers = [69] #, [15, 22,33,66/71,132/131/129]
-maxiter = [500_000] #[1M, 1M, 500k, 50k, 12k]
+numbers = [262,264] #, [15, 22,33,66/71,132/131/129]
+maxiter = [1_000] #[1M, 1M, 500k, 50k, 12k]
 L_list = [aux.lfy(N) for N in numbers]
 Y = 13
 
@@ -12,7 +12,7 @@ for i, N in enumerate(numbers):
     step = max(maxiter[i]//100, 50)
     print("from numpy import array", file=files[i])
     print("#step: " +str(step), file=files[i])
-    res = entanglement_entropy_montecarlo(Y, N, maxiter=maxiter[i], step=step, tol=1e-2, gpu=True, sparse=False)
+    res = entanglement_entropy_montecarlo(Y, N, maxiter=maxiter[i], step=step, tol=1e-2, gpu=False, sparse=False)
     print("results = " + str(res), file= files[i])
     files[i].close()
     print("lens: ", [len(r[1]) for r in res])
--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=N69_prec2_step_1_000      # The name of your job, you'll se it in squeue.
+#SBATCH --job-name=N262_264_prec2_step_50      # The name of your job, you'll se it in squeue.
 #SBATCH --mail-type=END              # Mail events (NONE, BEGIN, END, FAIL, ALL). Sends you an email when the job begins, ends, or fails; you can combine options.
 #SBATCH --mail-user=samadio@sissa.it    # Where to send the mail
 #
@@ -28,8 +28,8 @@
 # ---- Partition, Walltime and Output ----
 #
 #[unconfig] #SBATCH --array=01-10    # Create a job array. Useful for multiple, similar jobs. To use, read this: https://slurm.schedmd.com/job_array.html
-#SBATCH --partition=gpu1,gpu2         # Partition (queue). Avail: regular1, regular2, long1, long2, wide1, wide2, gpu1, gpu2. Multiple partitions are possible.
-#SBATCH --time=12:00:00              # Time limit hrs:min:sec
+#SBATCH --partition=long1,long2         # Partition (queue). Avail: regular1, regular2, long1, long2, wide1, wide2, gpu1, gpu2. Multiple partitions are possible.
+#SBATCH --time=48:00:00              # Time limit hrs:min:sec
 #SBATCH --output=%j.o%j              # Standard output log in TORQUE-style -- WARNING: %x requires a new enough SLURM. Use %j for regular jobs and %A-%a for array jobs
 #SBATCH --error=%j.e%j               # Standard error  log in TORQUE-style -- WARNING: %x requires a new enough SLURM. Use %j for regular jobs and %A-%a for array jobs
 
