f89aef5ab2536c1e9c13194ffac06bebf8968a8c
Simone Amadio <amadiosimone1996@gmail.com>
2020-09-30 11:22:06 +0200

    AllY, some missing

---
+++ Entropy/src/single_N_multiple_Y_before_IQFT.py
@@ -0,0 +1,26 @@
+from entropies import montecarlo_simulation
+from states import *
+from auxiliary.auxiliary import get_candidates
+from math import gcd as GCD
+
+
+N = 123
+L = aux.lfy(N)
+Y_list = [i for i in range(2,N) if GCD(i,N)==1]
+maxiter = 500_000
+
+with open("/home/samadio/entanglement_entropy/Entropy/1_percent/all_Y_N_"+str(N)+".py", 'a+') as file:
+
+    step=1_000
+
+    for Y in Y_list:
+        nonzeros = aux.nonzeros_decimal(2 * L,Y,N)
+        bipartitions = [bip.random_bipartition(range(3 * L), (3 * L) // 2) for j in range(maxiter)]
+        current_state = construct_modular_state(2*L,L,nonzeros).toarray().reshape(2**(3*L))
+
+        flags, entropies = montecarlo_simulation(current_state, step, maxiter, bipartitions, tol=1e-2, gpu=True, sparse=False)
+
+        if flags[0] and flags[1]:
+                single_Y_mean = np.mean(entropies)
+                print("(" +str(Y)+ ", " +str(single_Y_mean)+ ")", file=file)
+        else: print(str(Y)+" not converged")
--- Entropy/src/test.py
+++
@@ -1,9 +0,0 @@
-from auxiliary.auxiliary import prime_power as pp
-from auxiliary.auxiliary import get_candidates as gc
-
-b = gc(13,65,128)
-print(b)
-
-a = gc(13,127,256)
-print(a)
-print(len(a))
