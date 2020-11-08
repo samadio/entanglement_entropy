9b6593769f871bfadf771cdc6531005b08736919
Simone Amadio <amadiosimone1996@gmail.com>
2020-11-08 20:57:38 +0100

    Compatibility update

--- Entropy/src/main_cuda_dense_montecarlo.py
+++ Entropy/src/main_cuda_dense_montecarlo.py
@@ -5,21 +5,21 @@ from auxiliary.auxiliary import get_candidates
 
 Y=13
 #numbers = get_candidates(13,129,256)[24:] #[15, 22,33,66/71,132/131/129]
-numbers = [21]
-maxiter = [100] #[1M, 1M, 500k, 50k, 12k]
+numbers = [75]
+maxiter = [50_000] #[1M, 1M, 500k, 50k, 12k]
 L_list = [aux.lfy(N) for N in numbers]
 
-files = [open("/home/samadio/entanglement_entropy/Entropy/1_percent/results_Y_" + str(Y) + "_N_"+str(N)+".py", 'a+') for i,N in enumerate(numbers)]
+files = [open("/home/samadio/entanglement_entropy/Entropy/1_percent/results_13_75_compat.py", 'a+') for i,N in enumerate(numbers)]
 
 for i, N in enumerate(numbers):
     #step = max(maxiter[0]//50, 50)
-    step=10
+    step=1_000
 
     print("from numpy import array", file=files[i])
     print("#step: " +str(step), file=files[i])
-    res = entanglement_entropy_montecarlo(Y, N, maxiter=maxiter[0], step=step, tol=1e-2, gpu=False, sparse=False)
+    res = entanglement_entropy_montecarlo(Y, N, maxiter=maxiter[0], step=step, tol=1e-2, gpu=True, sparse=False)
     print("results = " + str(res), file= files[i])
     files[i].close()
-    if i == 0: print("lens: ", [len(r[1]) for r in res])
+    #if i == 0: print("lens: ", [len(r[1]) for r in res])
     #print("convergence: ", [r[0] for r in res])
     #print(str(N) + "done")
