92793f2ca58d186330253bb5d110003d596e0ba6
Simone Amadio <amadiosimone1996@gmail.com>
2020-11-09 08:05:33 +0100

    Montecarlo data update

--- Entropy/src/main_cuda_dense_montecarlo.py
+++ Entropy/src/main_cuda_dense_montecarlo.py
@@ -5,15 +5,15 @@ from auxiliary.auxiliary import get_candidates
 
 Y=13
 #numbers = get_candidates(13,129,256)[24:] #[15, 22,33,66/71,132/131/129]
-numbers = [75]
-maxiter = [50_000] #[1M, 1M, 500k, 50k, 12k]
+numbers = [255]
+maxiter = [12_000] #[1M, 1M, 500k, 50k, 12k]
 L_list = [aux.lfy(N) for N in numbers]
+step = 1_500
 
-files = [open("/home/samadio/entanglement_entropy/Entropy/1_percent/results_13_75_compat.py", 'a+') for i,N in enumerate(numbers)]
+files = [open("/home/samadio/entanglement_entropy/Entropy/1_percent/results_13_255_"+str(step)+".py", 'a+') for i,N in enumerate(numbers)]
 
 for i, N in enumerate(numbers):
     #step = max(maxiter[0]//50, 50)
-    step=1_000
 
     print("from numpy import array", file=files[i])
     print("#step: " +str(step), file=files[i])
--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=N75_compat      # The name of your job, you'll se it in squeue.
+#SBATCH --job-name=N255      # The name of your job, you'll se it in squeue.
 #SBATCH --mail-type=END              # Mail events (NONE, BEGIN, END, FAIL, ALL). Sends you an email when the job begins, ends, or fails; you can combine options.
 #SBATCH --mail-user=samadio@sissa.it    # Where to send the mail
 #
